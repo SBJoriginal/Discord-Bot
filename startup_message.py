efbc390286c58a3ed4e66b766212d1486896c728
Lenny Plante <lsbjoriginal@gmail.com>
2024-11-19 12:50:46 -0500

    Update startup_message.py

--- startup_message.py
+++ startup_message.py
@@ -3,5 +3,5 @@ import discord
 
 async def opening_message(channel):
     # Send a message to the specified channel
-    await channel.send("I am now connected to the cloud! That means I am always available")
+    # await channel.send("I am now connected to the cloud! That means I am always available")
     print(f"Opening message sent to channel: {channel.name}")
